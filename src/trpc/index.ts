05473cf2455a3962a082f4ab41b09e8a8586c0e6
Sushant <samheuer26433@gmail.com>
2024-01-17 22:33:14 +0530

    Users can now be synced to db

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
@@ -8,7 +9,21 @@ export const appRouter = router({
     if (!user || !user.id || !user.email) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
     }
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
 
+    if (!dbUser) {
+      // Found a new user
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
     return { success: true };
   }),
 });
