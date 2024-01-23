233ea4c0b67b4a3dcb39cf702a1d0549cf3c5162
Sushant <samheuer26433@gmail.com>
2024-01-23 23:26:53 +0530

    Added progress bar to PDF dropzone

--- src/components/UploadDropzone.tsx
+++ src/components/UploadDropzone.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import { Cloud, File } from "lucide-react";
+import React, { useState } from "react";
 import Dropzone from "react-dropzone";
+import { Progress } from "./ui/progress";
 
 function UploadDropzone() {
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const startSimulatedProgress = () => {
+    setUploadProgress(0);
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
+        }
+        return prevProgress + 5;
+      });
+    }, 500);
+    return interval;
+  };
+
   return (
     <Dropzone
       multiple={false}
       onDrop={(acceptedFiles) => {
-        console.log(acceptedFiles);
+        setIsUploading(true);
+
+        const progressInterval = startSimulatedProgress();
+
+        // TODO : Handle file upload
+
+        clearInterval(progressInterval);
+        setUploadProgress(100);
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -19,7 +45,34 @@ function UploadDropzone() {
               htmlFor="dropzone-file"
               className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
             >
-              <div className="flex flex-col items-center justify-center pt-5 pb-6"></div>
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Click to upload</span>
+                  {` `}
+                  or drag & drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF (Upto 4 MB)</p>
+              </div>
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <File className="h-4 w-4 text-blue-500" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress
+                    value={uploadProgress}
+                    className="h-1 w-4 bg-zinc-200"
+                  />
+                </div>
+              ) : null}
             </label>
           </div>
         </div>
