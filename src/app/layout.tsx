e6c8847d206bba6da0284cc0bd3524da0c190b1c
Sushant <samheuer26433@gmail.com>
2024-01-25 22:46:45 +0530

    Added support for toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import NavBar from "@/components/NavBar";
 import "react-loading-skeleton/dist/skeleton.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           "min-h-screen font-sans antialiased grainy"
         )}
       >
+        <Toaster />
         <NavBar />
         {children}
       </body>
